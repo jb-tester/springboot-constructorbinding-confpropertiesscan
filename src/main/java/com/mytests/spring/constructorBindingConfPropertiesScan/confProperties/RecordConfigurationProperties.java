dc7f73b62e3b368ec2ab95af568a23a3cc113de2
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2022-07-15 11:11:43 +0200

    records conf properties

--- src/main/java/com/mytests/spring/constructorBindingConfPropertiesScan/confProperties/RecordConfigurationProperties.java
+++ src/main/java/com/mytests/spring/constructorBindingConfPropertiesScan/confProperties/RecordConfigurationProperties.java
@@ -1,7 +1,13 @@
 package com.mytests.spring.constructorBindingConfPropertiesScan.confProperties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+
+import java.util.List;
 
 
 @ConfigurationProperties(prefix = "my.props4")
-public record RecordConfigurationProperties(String prop1, String prop2) { }
+public record RecordConfigurationProperties(String prop1,
+                                            String prop2,
+                                            @NestedConfigurationProperty UtilsRecord utilsRecord,
+                                            List<UtilsRecord> recordsList) { }
---
+++ src/main/java/com/mytests/spring/constructorBindingConfPropertiesScan/confProperties/UtilsRecord.java
@@ -0,0 +1,13 @@
+package com.mytests.spring.constructorBindingConfPropertiesScan.confProperties;
+
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+
+/**
+ * *
+ * <p>Created by irina on 7/15/2022.</p>
+ * <p>Project: springboot-constructorbinding-confpropertiesscan</p>
+ * *
+ */
+
+public record UtilsRecord(String nestedProp1) {
+}
